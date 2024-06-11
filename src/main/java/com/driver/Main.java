dd075f60797a3788bcac9e539548fcc8d27ef611
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-06-11 13:24:10 +0530

    Assignment done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,25 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+        A obj = new B();
+        System.out.print(obj.meth());
+    }
+
+    public static class A {
+
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+
+    }
   
 }
